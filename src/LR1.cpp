7d0226f946224e5c43caf7a598e7ceceac9f8a0d
yury <imelya@bk.ru>
2017-04-20 11:07:37 +0300

    add isUint in LR1

--- src/LR1.cpp
+++ src/LR1.cpp
@@ -1,73 +1,85 @@
 /*Опишите параметризованную функцию инверсии массива элементов. Продемонстрируйте использование функции для разных типов данных.
 */
 #include <iostream>
+#include <sstream>
 #include "LR1task.h"
 #include "helpFunctions.h"
 
 int LR1() {
-	std::cout << "==LR1==\n";
-	char answer;
-    uint s;
-	std::cout<<"SizeOfIntArray: ";
-    std::cin>>s;
-    if(!std::cin) {
-        std::cout << "Need number" << "\n";
-        }
-	else {
-    if (!check2(s)) {
-    	std::cout << "Less then 2. Try again? (y/n)\n";
-		std::cin >> answer;
-		if (answer == 'y') {
-			answer = ' ';
-			LR1();
+	uint s = 0;
+	std::string input;
+	char answer = ' ';
+	do {
+		std::cout << "==LR1==\n";
+		std::cout<<"SizeOfIntArray: ";
+		std::cin >> input;
+		if (!isUint(input)) { 
+			std::cout << "Need number. Try agan? (y/n)\n";
+			std::cin >> answer;
+			if (answer == 'y'){
+				continue;
+			}
+			else {
+				return 0;
+			}
 		}
 		else {
-			return 0;
+			std::stringstream it(input);
+			it >> s;
+			if (!check2(s)) {
+				std::cout << "Less then 2. Try again? (y/n)\n";
+				std::cin >> answer;
+				if (answer == 'y') {
+					continue;
+				}
+				else {
+					return 0;
+				}
+			}
+			std::vector<int> vec(s);
+			std::cout << "Array: ";
+				for(uint i = 0; i < vec.size(); i++) {
+					vec[i] = i;
+					std::cout << vec[i] << ' ';
+				}
+			std::vector<int> new_vec = invert(vec);
+			std::cout << "\nInvertArray: ";
+				for (uint k = 0; k < s; k++) {
+					std::cout <<new_vec[k] << " ";
+				}
+			std::cout << "\n";
+
+			std::vector<unsigned char>vect;
+				for(size_t i = 0; i < s; ++i ) {
+					char sym = 'a';
+					sym += i;
+					vect.push_back(sym);
+				}
+
+			//cout<<"Code a: "<<(int) 'a' <<"\n";
+			//cout<<"Code A -> a: "<<(char) ('A' + 32) <<"\n";
+
+			std::cout << "\n";
+			std::cout << "ArrayStr: ";
+				for(uint i=0; i<vect.size(); ++i) {
+					std::cout <<vect[i] << " ";
+				}
+			std::vector<unsigned char> new_vect = invert(vect);
+			std::cout << "\nInvertArrayStr: ";
+				for(uint i=0; i<vect.size(); ++i) {
+					std::cout << vect[i] << " ";
+				}
+			std::cout << "\n";
+			std::cout << "Return to main menu? (y/n)\n";
+			std::cin >> answer;
+			if (answer == 'y') {
+				answer = ' ';
+				main();
+			}
+			else 
+				return 0;
 		}
 	}
-    std::vector<int> vec(s);
-    std::cout << "Array: ";
-        for(uint i = 0; i < vec.size(); i++) {
-			vec[i] = i;
-            std::cout << vec[i] << ' ';
-        }
-	std::vector<int> new_vec = invert(vec);
-    std::cout << "\nInvertArray: ";
-        for (uint k = 0; k < s; k++) {
-            std::cout <<new_vec[k] << " ";
-        }
-    std::cout << "\n";
-
-	std::vector<unsigned char>vect;
-        for(size_t i = 0; i < s; ++i ) {
-            char sym = 'a';
-            sym += i;
-			vect.push_back(sym);
-        }
-
-    //cout<<"Code a: "<<(int) 'a' <<"\n";
-    //cout<<"Code A -> a: "<<(char) ('A' + 32) <<"\n";
-
-    std::cout << "\n";
-    std::cout << "ArrayStr: ";
-        for(uint i=0; i<vect.size(); ++i) {
-            std::cout <<vect[i] << " ";
-        }
-	std::vector<unsigned char> new_vect = invert(vect);
-    std::cout << "\nInvertArrayStr: ";
-        for(uint i=0; i<vect.size(); ++i) {
-            std::cout << vect[i] << " ";
-        }
-    std::cout << "\n";
-	
-	std::cout << "Return to main menu? (y/n)\n";
-	std::cin >> answer;
-	if (answer == 'y') {
-		answer = ' ';
-		main();
-	}
-	else 
-		return 0;
-    	}
+	while (answer == 'y'); 
     return 0;
 }
